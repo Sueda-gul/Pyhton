feb12993c28cf3e1540749cfda48bd7f04215eb3
Sueda-gul <suedagul.48@gmail.com>
2021-05-13 02:37:25 +0300

    130521-0237

---
+++ 02_karar_yapilari/0202_karsilastirma_operatorleri.py
@@ -0,0 +1,75 @@
+#region karsilastirma_operatorleri
+"""
+1→ == eşittir
+2→ != eşit değildir, visualbasic'te <> şeklinde
+3→ < küçüktür
+4→ > büyüktür
+5→ <= küçük eşittir
+6→ >= büyük eşittir
+"""
+#endregion
+#region ==
+"""
+print(10==10)
+print(-5==-5)
+print(10==5)
+print("istanbul" == "istanbul")
+print("istanbul" == "İstanbul")
+print(""=="")
+print("meb" == "MEB")
+"""
+#endregion
+#region !=
+"""
+print(10 != 10)
+print(10 != 5)
+print("meb" != "MEB")
+"""
+#endregion
+#region <
+"""
+print(10 < 20)
+print(10 < 10)
+print(5 < 9)
+print("esra" < "es")
+"""
+#endregion
+#region >
+"""
+print(10 > 20)
+print(10 > 10)
+print(10 > 5)
+"""
+#endregion
+#region <=
+"""
+print(10 >= 20)
+print(10 >= 10)
+print(10 >= 5)
+"""
+#endregion
+#region >=
+"""
+print(10 >= 20)
+print(10 >= 10)
+print(10 >= 5)
+"""
+#endregion
+#region ornek_1
+"""
+ogrenci = int(input("Lütfen Öğrencinin Yaşını Giriniz : "))
+print(ogrenci >= 18)
+"""
+#endregion
+#region ornek_2
+"""
+Kullanıcı Adını Giriniz : admin   → True
+Kullanıcı Adını Giriniz : user    → False
+
+uName = input("Lütfen Kullanıcı Adı Giriniz : ")
+print(uName == "admin")
+"""
+#endregion
+
+
+
