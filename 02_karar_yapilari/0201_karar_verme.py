9cc4a1fc3941bf0cdddbb826c8eba9ca665bb023
Sueda-gul <suedagul.48@gmail.com>
2021-05-12 17:22:34 +0300

    120521-1722

---
+++ 02_karar_yapilari/0201_karar_verme.py
@@ -0,0 +1,15 @@
+#region karar_verme_giris
+#şu ana kadar ne yaptık?
+"""
+1 - Aritmetik İşlemler
+2 - Kullanıcıdan veri girişi
+3 - Doğru formatta ekrana yazdır
+
+"""
+#endregion
+#region karar_verme_asamasi
+"""
+karar vermeyi nasıl yapacak?
+Karşılaştırma operatörleri ile
+"""
+#endregion
